09eef4c32dcd70552a8b2e0c5dd4a84ba3d08078
Willem Boshoff <willem.boshoff@dyna-mo.com>
2021-07-02 09:21:40 +0200

    Add init.el

---
+++ init.el
@@ -0,0 +1,142 @@
+;; Basic window look and functionality
+(setq inhibit-startup-message t)
+(scroll-bar-mode -1) ;; Disable visible scrollbar
+(tool-bar-mode -1) ;; Disable the toolbar
+(tooltip-mode -1) ;; Disable tooltips
+(set-fringe-mode 10) ;; Give some breathing room
+(menu-bar-mode -1) ;; Disable the menu bar
+(setq visible-bell t) ;; Disable bell sounds
+
+;; Font
+(set-face-attribute 'default nil :font "CaskaydiaCove Nerd Font" :height 90)
+
+;; Enable line numbers
+(column-number-mode)
+(global-display-line-numbers-mode t)
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+		 term-mode-hook
+		 eshell-mode-hook))
+	 (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("melpa-stable" . "https://stable.melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; Log any commands pressed, and their description
+(use-package command-log-mode)
+
+;; Fuzzy search
+(use-package swiper)
+
+;; Code-completion package
+(use-package ivy
+  :bind (("C-s" . swiper))
+  :config
+  (ivy-mode 1))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history)))
+
+;; Cool icons
+(use-package all-the-icons)
+
+;; Custom themes
+(use-package doom-themes
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-dracula t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+;; Better modeline
+(use-package doom-modeline
+  :hook (after-init . doom-modeline-mode))
+
+;; Brackets highlighting
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; Highlight matching braces
+(use-package paren
+  :config
+  (set-face-attribute 'show-paren-match-expression nil :background "#363e4a")
+  (show-paren-mode 1))
+
+;; Possible remaining key bindings
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+;; Better help documentation
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . helpful-function)
+  ([remap describe-symbol] . helpful-symbol)
+  ([remap describe-variable] . helpful-variable)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-key] . helpful-key))
+
+;; Better projects functionality
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (when (file-directory-p "C:\git")
+    (setq projectile-project-search-path '("C:\git")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("3db307fb06cedec4f2f6dfcbc189ffc26bca9653d7e149643d451b8411a8f039" default))
+ '(package-selected-packages
+   '(counsel-projectile projectile helpful counsel doom-themes minions ivy-rich which-key rainbow-delimiters use-package tree-sitter-langs tree-sitter-indent swiper omnisharp kubernetes k8s-mode git doom-modeline dockerfile-mode docker-compose-mode docker-cli docker dashboard command-log-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
