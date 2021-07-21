ee8a3cebe04c5b9f3f12b834ae893923e4e62075
Willem Boshoff <boshoffwillem@localhost.localdomain>
2021-07-21 09:19:39 +0200

    Update init.el

--- init-old.el
+++ init-old.el
@@ -12,6 +12,15 @@
   (load bootstrap-file nil 'nomessage))
 (setq package-enable-at-startup nil)
 
+;; Include the following code in the emacs main init.el
+;; Do not use `init.el` for `custom-*` code - use `custom-file.el`.
+;;(setq custom-file "~/.emacs.d/custom-file.el")
+
+;; Assuming that the code in custom-file is execute before the code
+;; ahead of this line is not a safe assumption. So load this file
+;; proactively.
+;;(load-file custom-file);
+
 ;; Basic UI Configuration ------------------------------------------------------
 
 ;; Basic window look and functionality
@@ -19,7 +28,7 @@
 (scroll-bar-mode -1) ;; Disable visible scrollbar
 (tool-bar-mode -1) ;; Disable the toolbar
 (tooltip-mode -1) ;; Disable tooltips
-(set-fringe-mode 30) ;; Give some breathing room
+(set-fringe-mode 10) ;; Give some breathing room
 (menu-bar-mode -1) ;; Disable the menu bar
 (setq visible-bell t) ;; Disable bell sounds
 
@@ -33,80 +42,58 @@
 		treemacs-mode-hook
 		eshell-mode-hook))
 	 (add-hook mode (lambda () (display-line-numbers-mode 0))))
-;; ----------------------------------------------------------------------------
 
 ;; Font Configuration ----------------------------------------------------------
 
 ;; Font
-(set-face-attribute 'default nil :font "JetBrainsMono Nerd Font" :height 95 :weight 'regular)
+(set-face-attribute 'default nil :font "JetBrainsMono Nerd Font" :height 93 :weight 'regular)
 
 ;; Set the fixed pitch face
-(set-face-attribute 'fixed-pitch nil :font "JetBrainsMono Nerd Font" :height 95 :weight 'regular)
+(set-face-attribute 'fixed-pitch nil :font "JetBrainsMono Nerd Font" :height 93 :weight 'regular)
 
 ;; Set the variable pitch face
-(set-face-attribute 'variable-pitch nil :font "Iosevka Nerd Font" :height 110 :weight 'regular)
-
-;; -------------------------------------------------------------------------------
+(set-face-attribute 'variable-pitch nil :font "Iosevka Nerd Font" :height 100 :weight 'regular)
 
 ;; Package Manager Configuration -----------------------------------------------
 
+;; Initialize package sources
 (require 'package)
-(add-to-list 'package-archives
-             '(("melpa" . "https://melpa.org/packages/")
-	       ("melpa-stable" . "https://stable.melpa.org/packages/")
-	       ("org" . "https://orgmode.org/elpa/")
-	       ("elpa" . "https://elpa.gnu.org/packages/")))
 
-(straight-use-package 'use-package)
-(setq straight-use-package-by-default t)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("melpa-stable" . "https://stable.melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
 
-;; ----------------------------------------------------------------------------
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
-;; Better EMACS functionality and help --------------------------------------
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; Log any commands pressed, and their description
+(use-package command-log-mode)
+
+;; Ivy Configuration -----------------------------------------------------------
+
+;; Fuzzy search
+(use-package swiper)
+
+;; Code-completion package
 (use-package ivy
-  :init
-  (ivy-mode 1)
-  (setq ivy-height 12
-        ivy-fixed-height-minibuffer t
-        ivy-use-virtual-buffers t)
-  :bind (("C-x b" . ivy-switch-buffer)
-         ("C-c r" . ivy-resume)
-         ("C-x C-b" . ivy-switch-buffer)))
+  :bind (("C-s" . swiper))
+  :config
+  (ivy-mode 1))
 
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
-         ("C-x C-f" . counsel-find-file)
-         ("C-c g" . counsel-ag)
-	 ("C-c b" . counsel-ibuffer)
-         ("C-c G" . counsel-git)
-         ("M-y" . counsel-yank-pop)
-         ("C-c C-j" . counsel-imenu)))
-
-(use-package swiper
-  :bind ("C-c s" . swiper))
-
-;; Possible remaining key bindings
-(use-package which-key
-  :init (which-key-mode)
-  :diminish which-key-mode
-  :config
-  (setq which-key-idle-delay 0.3))
-
-;; Better help documentation
-(use-package helpful
-  :custom
-  (counsel-describe-function-function #'helpful-callable)
-  (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ([remap describe-function] . helpful-function)
-  ([remap describe-symbol] . helpful-symbol)
-  ([remap describe-variable] . helpful-variable)
-  ([remap describe-command] . helpful-command)
-  ([remap describe-key] . helpful-key))
-
-;; ----------------------------------------------------------------
-
-;; Themes, icons, visuals, etc. ---------------------------------
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history)))
 
 ;; NOTE: The first time you load your configuration on a new machine, you'll
 ;; need to run the following command interactively so that mode line icons
@@ -149,19 +136,28 @@
   (set-face-attribute 'show-paren-match-expression nil :background "#363e4a")
   (show-paren-mode 1))
 
-;; --------------------------------------------------------------
-
-;; Eshell -----------------------------------------------------------
-
-(use-package eshell-git-prompt)
-
-(use-package eshell
+;; Possible remaining key bindings
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
   :config
-  (eshell-git-promt-use-theme 'powerline))
-;; ------------------------------------------------------------------
+  (setq which-key-idle-delay 0.3))
 
-;; IDE functionality ------------------------------------------------
+;; Better help documentation
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . helpful-function)
+  ([remap describe-symbol] . helpful-symbol)
+  ([remap describe-variable] . helpful-variable)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-key] . helpful-key))
 
+;; Projectile Configuration ----------------------------------------------------
+
+;; Better projects functionality
 (use-package projectile
   :diminish projectile-mode
   :config (projectile-mode)
@@ -176,38 +172,16 @@
 (use-package counsel-projectile
   :config (counsel-projectile-mode))
 
+;; Magit Configuration ---------------------------------------------------------
+
+;; Git
 (use-package magit
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
-(use-package flycheck
-  :init (global-flycheck-mode))
+;; Org Mode Configuration ------------------------------------------------------
 
-(defun wb/lsp-mode-setup ()
-  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
-  (lsp-headerline-breadcrumb-mode))
-
-(use-package lsp-mode
-  :init
-  (setq lsp-keymap-prefix "C-c l")
-  :config (lsp-enable-which-key-integration t)
-  :commands (lsp lsp-deferred)
-  :hook (lsp-mode . wb/lsp-mode-setup))
-
-(use-package lsp-ui
-  :hook (lsp-mode . lsp-ui-mode)
-  :custom
-  (lsp-ui-doc-position 'bottom))
-
-(use-package lsp-treemacs
-  :after lsp)
-
-(use-package lsp-ivy)
-
-;; ------------------------------------------------------------------
-
-;; ORG mode --------------------------------------------------------
-
+;; Org mode
 (defun wb/org-mode-setup ()
   (org-indent-mode)
   (variable-pitch-mode 1)
@@ -265,4 +239,81 @@
 (use-package visual-fill-column
   :hook (org-mode . wb/org-mode-visual-fill))
 
-;; ----------------------------------------------------------------
+;;IDE setup
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+(defun wb/lsp-mode-setup ()
+  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
+  (lsp-headerline-breadcrumb-mode))
+
+(use-package lsp-mode
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :config (lsp-enable-which-key-integration t)
+  :commands (lsp lsp-deferred)
+  :hook (lsp-mode . wb/lsp-mode-setup))
+
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode)
+  :custom
+  (lsp-ui-doc-position 'bottom))
+
+(use-package lsp-treemacs
+  :after lsp)
+
+(use-package lsp-ivy)
+
+;; Programming language support -----------------------------------------
+;;(use-package rust-mode
+;;  :hook (rust-mode . cargo-minor-mode)
+;;  :config
+;;  (setq indent-tabs-mode nil)
+;;  (setq rust-format-on-save t)
+;;  (setq rust-mode-map "C-c C-c"))
+
+(straight-use-package 'rustic)
+
+(use-package typescript-mode
+  :mode "\\.ts\\'"
+  :hook (typescript-mode . lsp-deferred)
+  :config
+  (setq typescript-indent-level 2))
+
+;;(setq omnisharp-server-executable-path "~/bin/omnisharp-linux-x86/omnisharp/OmniSharp.exe")
+(use-package csharp-mode
+  :mode "\\.cs\\'"
+  :hook (csharp-mode . lsp-deferred)
+  :config
+  (setq csharp-indent-level 4))
+
+;; Code completion
+(use-package company
+  :after lsp-mode
+  :hook (lsp-mode . company-mode)
+  :bind (:map company-active-map
+         ("<tab>" . company-complete-selection))
+        (:map lsp-mode-map
+         ("<tab>" . company-indent-or-complete-common))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.0))
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+;; -----------------------------------------
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(delete-selection-mode nil)
+ '(package-selected-packages
+   '(gnu-elpa-keyring-update rustic xterm-color which-key visual-fill-column use-package typescript-mode rainbow-delimiters racer project org-bullets omnisharp magit lsp-ui lsp-treemacs lsp-ivy helpful doom-themes doom-modeline counsel-projectile company-box command-log-mode cargo)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
