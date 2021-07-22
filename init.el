3d2acb289c08d5a4e22115aee39cd9e803832220
Willem Boshoff <willem.boshoff@dyna-mo.com>
2021-07-22 16:36:10 +0200

    New init.el

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+;; -------------------------------------------------------------------------------------
+;; Implement straight package-management system
+
+(setq package-enable-at-startup nil)
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -10,11 +14,19 @@
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
-(setq package-enable-at-startup nil)
+(straight-use-package 'use-package)
+(cl-dolist (mode '(emacs-lisp-mode lisp-interaction-mode))
+  (font-lock-add-keywords
+   mode
+   '(("(\\<\\(straight-use-package\\)\\>" 1 font-lock-keyword-face))))
+(setq straight-use-package-by-default 1)
 
-;; Basic UI Configuration ------------------------------------------------------
+;; -------------------------------------------------------------------------------------
 
-;; Basic window look and functionality
+;; -------------------------------------------------------------------------------------
+;; Modify base emacs window,functionality and look-and-feel
+;; -------------------------------------------------------------------------------------
+
 (setq inhibit-startup-message t)
 (scroll-bar-mode -1) ;; Disable visible scrollbar
 (tool-bar-mode -1) ;; Disable the toolbar
@@ -33,9 +45,6 @@
 		treemacs-mode-hook
 		eshell-mode-hook))
 	 (add-hook mode (lambda () (display-line-numbers-mode 0))))
-;; ----------------------------------------------------------------------------
-
-;; Font Configuration ----------------------------------------------------------
 
 ;; Font
 (set-face-attribute 'default nil :font "JetBrainsMono Nerd Font" :height 95 :weight 'regular)
@@ -46,68 +55,6 @@
 ;; Set the variable pitch face
 (set-face-attribute 'variable-pitch nil :font "Iosevka Nerd Font" :height 110 :weight 'regular)
 
-;; -------------------------------------------------------------------------------
-
-;; Package Manager Configuration -----------------------------------------------
-
-(require 'package)
-(add-to-list 'package-archives
-             '(("melpa" . "https://melpa.org/packages/")
-	       ("melpa-stable" . "https://stable.melpa.org/packages/")
-	       ("org" . "https://orgmode.org/elpa/")
-	       ("elpa" . "https://elpa.gnu.org/packages/")))
-
-(straight-use-package 'use-package)
-(setq straight-use-package-by-default t)
-
-;; ----------------------------------------------------------------------------
-
-;; Better EMACS functionality and help --------------------------------------
-(use-package ivy
-  :init
-  (ivy-mode 1)
-  (setq ivy-height 12
-        ivy-fixed-height-minibuffer t
-        ivy-use-virtual-buffers t)
-  :bind (("C-x b" . ivy-switch-buffer)
-         ("C-c r" . ivy-resume)
-         ("C-x C-b" . ivy-switch-buffer)))
-
-(use-package counsel
-  :bind (("M-x" . counsel-M-x)
-         ("C-x C-f" . counsel-find-file)
-         ("C-c g" . counsel-ag)
-	 ("C-c b" . counsel-ibuffer)
-         ("C-c G" . counsel-git)
-         ("M-y" . counsel-yank-pop)
-         ("C-c C-j" . counsel-imenu)))
-
-(use-package swiper
-  :bind ("C-c s" . swiper))
-
-;; Possible remaining key bindings
-(use-package which-key
-  :init (which-key-mode)
-  :diminish which-key-mode
-  :config
-  (setq which-key-idle-delay 0.3))
-
-;; Better help documentation
-(use-package helpful
-  :custom
-  (counsel-describe-function-function #'helpful-callable)
-  (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ([remap describe-function] . helpful-function)
-  ([remap describe-symbol] . helpful-symbol)
-  ([remap describe-variable] . helpful-variable)
-  ([remap describe-command] . helpful-command)
-  ([remap describe-key] . helpful-key))
-
-;; ----------------------------------------------------------------
-
-;; Themes, icons, visuals, etc. ---------------------------------
-
 ;; NOTE: The first time you load your configuration on a new machine, you'll
 ;; need to run the following command interactively so that mode line icons
 ;; display correctly:
@@ -117,7 +64,23 @@
 ;; Cool icons
 (use-package all-the-icons)
 
-;; Custom themes
+;; Brackets highlighting
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; Highlight matching braces
+(use-package paren
+  :config
+  (set-face-attribute 'show-paren-match-expression nil :background "#363e4a")
+  (show-paren-mode 1))
+
+;; Possible remaining key bindings
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
 (use-package doom-themes
   :config
   ;; Global settings (defaults)
@@ -135,78 +98,42 @@
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
 
-;; Better modeline
 (use-package doom-modeline
   :hook (after-init . doom-modeline-mode))
 
-;; Brackets highlighting
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
+;; -------------------------------------------------------------------------------------
+;; Better searching, completions and help
 
-;; Highlight matching braces
-(use-package paren
-  :config
-  (set-face-attribute 'show-paren-match-expression nil :background "#363e4a")
-  (show-paren-mode 1))
-
-;; --------------------------------------------------------------
-
-;; Eshell -----------------------------------------------------------
-
-(use-package eshell-git-prompt)
-
-(use-package eshell
-  :config
-  (eshell-git-promt-use-theme 'powerline))
-;; ------------------------------------------------------------------
-
-;; IDE functionality ------------------------------------------------
-
-(use-package projectile
-  :diminish projectile-mode
-  :config (projectile-mode)
-  :custom ((projectile-completion-system 'ivy))
-  :bind-keymap
-  ("C-c p" . projectile-command-map)
-  :init
-  (when (file-directory-p "~/git/")
-    (setq projectile-project-search-path '("~/git/")))
-  (setq projectile-switch-project-action #'projectile-dired))
-
-(use-package counsel-projectile
-  :config (counsel-projectile-mode))
-
-(use-package magit
+(use-package helpful
   :custom
-  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . helpful-function)
+  ([remap describe-symbol] . helpful-symbol)
+  ([remap describe-variable] . helpful-variable)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-key] . helpful-key))
 
-(use-package flycheck
-  :init (global-flycheck-mode))
+(use-package swiper)
 
-(defun wb/lsp-mode-setup ()
-  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
-  (lsp-headerline-breadcrumb-mode))
+(use-package ivy
+  :bind (("C-s" . swiper))
+  :config
+  (ivy-mode 1))
 
-(use-package lsp-mode
-  :init
-  (setq lsp-keymap-prefix "C-c l")
-  :config (lsp-enable-which-key-integration t)
-  :commands (lsp lsp-deferred)
-  :hook (lsp-mode . wb/lsp-mode-setup))
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 ("M-y" . counsel-yank-pop)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history)))
 
-(use-package lsp-ui
-  :hook (lsp-mode . lsp-ui-mode)
-  :custom
-  (lsp-ui-doc-position 'bottom))
+;; -------------------------------------------------------------------------------------
 
-(use-package lsp-treemacs
-  :after lsp)
-
-(use-package lsp-ivy)
-
-;; ------------------------------------------------------------------
-
-;; ORG mode --------------------------------------------------------
+;; -------------------------------------------------------------------------------------
+;; Org-mode
 
 (defun wb/org-mode-setup ()
   (org-indent-mode)
@@ -265,4 +192,105 @@
 (use-package visual-fill-column
   :hook (org-mode . wb/org-mode-visual-fill))
 
-;; ----------------------------------------------------------------
+;; -------------------------------------------------------------------------------------
+
+;; -------------------------------------------------------------------------------------
+;; IDE functionality
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom
+  (setq projectile-indexing-method 'alien
+	projectile-sort-order 'default
+	projectile-enable-caching t
+	projectile-completion-system 'ivy)
+  :bind (("C-c p" . projectile-command-map))
+  :init
+  (when (file-directory-p "~/git/")
+    (setq projectile-project-search-path '("~/git/")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+(use-package magit
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+;; ==================
+;; Code completion, indexing, intelli-sense, etc.
+
+(use-package flycheck
+  :init (global-flycheck-mode))
+
+(defun wb/lsp-mode-setup ()
+  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
+  (lsp-headerline-breadcrumb-mode)
+  (setq lsp-modeline-code-actions-segments '(count icon name))
+  (setq lsp-modeline-diagnostics-enable t))
+
+(use-package lsp-mode
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :commands lsp
+  :hook ((lsp-mode . wb/lsp-mode-setup)
+	 (csharp-mode . lsp-deferred)
+	 (lsp-mode . lsp-enable-which-key-integration)))
+
+(use-package lsp-ui :commands lsp-ui-mode)
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+(use-package lsp-treemacs
+  :commands lsp-treemacs-errors-list
+  :init
+  (lsp-treemacs-sync-mode 1))
+
+;; optionally if you want to use debugger
+(use-package dap-mode)
+;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+
+(use-package company
+  :after lsp-mode
+  :hook (lsp-mode . company-mode)
+  :bind (:map company-active-map
+	       ("<tab>" . company-complete-selection))
+         (:map lsp-mode-map
+	       ("<tab>" . company-indent-or-complete-common))
+  :custom
+  (setq company-minimum-prefix-length 1
+	company-idle-delay 0.0) ;; default is 0.2
+  )
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
+;; ==================
+
+;; ==================
+;; Specific support for various programming languages
+
+;; ````````````` Markup
+;; ````````````````````
+
+;; ````````````` XML
+;; `````````````````
+
+;; ````````````` C#
+(use-package csharp-mode
+  :mode "\\.cs\\'"
+  :config
+  (setq csharp-indent-level 4))
+;; ````````````````
+
+;; ````````````` Rust
+;; ``````````````````
+
+;; ````````````` Docker
+;; ````````````````````
+
+;; ````````````` YAML
+;; ``````````````````
+
+;; ==================
+
+;; -------------------------------------------------------------------------------------
