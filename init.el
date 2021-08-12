b76342694e5e2f063307015209ff8ed3284ccfab
Willem Boshoff <willem.boshoff@dyna-mo.com>
2021-08-12 13:25:29 +0200

    Init file generated with org babel.

--- init.el
+++ init.el
@@ -1,6 +1,3 @@
-;; -------------------------------------------------------------------------------------
-;; Implement straight package-management system
-
 (setq package-enable-at-startup nil)
 (defvar bootstrap-version)
 (let ((bootstrap-file
@@ -8,9 +5,9 @@
       (bootstrap-version 5))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
@@ -21,19 +18,27 @@
    '(("(\\<\\(straight-use-package\\)\\>" 1 font-lock-keyword-face))))
 (setq straight-use-package-by-default 1)
 
-;; -------------------------------------------------------------------------------------
+(setq inhibit-startup-message t) ;; Disable the startup message.
 
-;; -------------------------------------------------------------------------------------
-;; Modify base emacs window,functionality and look-and-feel
-;; -------------------------------------------------------------------------------------
+(scroll-bar-mode -1) ;; Disable visible scrollbar.
 
-(setq inhibit-startup-message t)
-(scroll-bar-mode -1) ;; Disable visible scrollbar
-(tool-bar-mode -1) ;; Disable the toolbar
-(tooltip-mode -1) ;; Disable tooltips
-(set-fringe-mode 30) ;; Give some breathing room
-(menu-bar-mode -1) ;; Disable the menu bar
-(setq visible-bell t) ;; Disable bell sounds
+(tooltip-mode -1) ;; Disable tooltips.
+
+(tool-bar-mode -1) ;; Disable the toolbar.
+
+(set-fringe-mode 30) ;; Give some breathing room.
+
+(menu-bar-mode -1) ;; Disable the menu bar.
+
+(setq visible-bell t) ;; Disable bell sounds.
+
+(delete-selection-mode 1) ;; Will replace highlighted text instead of inserting.
+
+(global-hl-line-mode +1) ;; Highlight the current line.
+
+(setq backup-directory-alist '(("." . "~/.emacs-backups"))) ;; Saves emacs backup files to a different directory.
+
+(global-set-key "\C-c\C-d" "\C-a\C- \C-n\M-w\C-y") ;; Shortcut to duplicate a line.
 
 ;; Enable line numbers
 (column-number-mode)
@@ -44,50 +49,40 @@
 		shell-mode-hook
 		treemacs-mode-hook
 		eshell-mode-hook))
-	 (add-hook mode (lambda () (display-line-numbers-mode 0))))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 ;; Font
 (set-face-attribute 'default nil :font "JetBrainsMono Nerd Font" :height 95 :weight 'regular)
-
 ;; Set the fixed pitch face
 (set-face-attribute 'fixed-pitch nil :font "JetBrainsMono Nerd Font" :height 95 :weight 'regular)
-
 ;; Set the variable pitch face
 (set-face-attribute 'variable-pitch nil :font "Iosevka Nerd Font" :height 110 :weight 'regular)
+
+(setq bookmark-default-file "~/.emacs.d/bookmarks")
+
+(setq bookmark-save-flag 1)
 
 ;; NOTE: The first time you load your configuration on a new machine, you'll
 ;; need to run the following command interactively so that mode line icons
 ;; display correctly:
-;;
 ;; M-x all-the-icons-install-fonts
-
 ;; Cool icons
 (use-package all-the-icons)
 
-;; Brackets highlighting
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
-
-;; Highlight matching braces
 (use-package paren
   :config
   (set-face-attribute 'show-paren-match-expression nil :background "#363e4a")
   (show-paren-mode 1))
 
-;; Possible remaining key bindings
-(use-package which-key
-  :init (which-key-mode)
-  :diminish which-key-mode
-  :config
-  (setq which-key-idle-delay 0.3))
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package doom-themes
   :config
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+	doom-themes-enable-italic t) ; if nil, italics is universally disabled
   (load-theme 'doom-dracula t)
-
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
   ;; Enable custom neotree theme (all-the-icons must be installed!)
@@ -101,9 +96,36 @@
 (use-package doom-modeline
   :hook (after-init . doom-modeline-mode))
 
-;; -------------------------------------------------------------------------------------
-;; Better searching, completions and help
+(use-package dashboard
+  :init
+  (progn
+    (setq dashboard-items '((recents . 5)
+			    (projects . 5)
+			    (bookmarks . 5)
+			    (agenda . 5)))
+    (setq dashboard-set-file-icons t)
+    (setq dashboard-set-heading-icons t)
+    )
+  :config
+  (dashboard-setup-startup-hook))
 
+(use-package centaur-tabs
+  :config
+  (setq centaur-tabs-set-bar 'under
+	centaur-tabs-set-icons t
+	centaur-tabs-gray-out-icons 'buffer
+	centaur-tabs-height 32
+	centaur-tabs-set-modified-marker t
+	centaur-tabs-modified-marker "*")
+  (centaur-tabs-headline-match)
+  (centaur-tabs-change-fonts "JetBrainsMono Nerd Font" 100)
+  (centaur-tabs-mode t))
+
+;; Show available key-strokes for currently typed commands
+(use-package which-key
+  :config (which-key-mode))
+
+;; Better documentation and helm information
 (use-package helpful
   :custom
   (counsel-describe-function-function #'helpful-callable)
@@ -115,182 +137,115 @@
   ([remap describe-command] . helpful-command)
   ([remap describe-key] . helpful-key))
 
-(use-package swiper)
-
-(use-package ivy
-  :bind (("C-s" . swiper))
+(use-package helm
+  :init
+  (require 'helm-config)
+  (setq helm-split-window-in-side-p t
+	helm-move-to-line-cycle-in-source t)
   :config
-  (ivy-mode 1))
-
-(use-package counsel
-  :bind (("M-x" . counsel-M-x)
-	 ("C-x b" . counsel-ibuffer)
-	 ("C-x C-f" . counsel-find-file)
-	 ("M-y" . counsel-yank-pop)
-	 :map minibuffer-local-map
-	 ("C-r" . 'counsel-minibuffer-history)))
-
-;; -------------------------------------------------------------------------------------
-
-;; -------------------------------------------------------------------------------------
-;; Org-mode
-
-(defun wb/org-mode-setup ()
-  (org-indent-mode)
-  (variable-pitch-mode 1)
-  (visual-line-mode 1))
-
-(defun wb/org-font-setup ()
-  ;; Replace list hyphen with dot
-  (font-lock-add-keywords 'org-mode
-                          '(("^ *\\([-]\\) "
-                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-
-(dolist (face '((org-level-1 . 1.2)
-                (org-level-2 . 1.1)
-                (org-level-3 . 1.05)
-                (org-level-4 . 1.0)
-                (org-level-5 . 1.1)
-                (org-level-6 . 1.1)
-                (org-level-7 . 1.1)
-                (org-level-8 . 1.1)))
-  (set-face-attribute (car face) nil :font "Iosevka Nerd Font" :weight 'regular :height (cdr face)))
-
-;; Ensure that anything that should be fixed-pitch in Org files appears that way
-(set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
-(set-face-attribute 'org-table nil  :inherit 'fixed-pitch)
-(set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
-(set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
-(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-(set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
-  
-(use-package org
-  :hook (org-mode . wb/org-mode-setup)
-  :config
-  (setq org-ellipsis " ↓")
-  (setq org-agenda-start-time-with-log-mode t)
-  (setq org-log-done 'time)
-  (setq org-log-into-drawer t)
-  (setq org-agenda-files
-	'("~/Documents/TODO.org"
-	  "~/Documents/Birthdays.org"))
-  (wb/org-font-setup))
-
-(use-package org-bullets
-  :after org
-  :hook (org-mode . org-bullets-mode)
-  :custom
-  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
-
-(defun wb/org-mode-visual-fill ()
-  (setq visual-fill-column-width 100
-        visual-fill-column-center-text t)
-  (visual-fill-column-mode 1))
-
-(use-package visual-fill-column
-  :hook (org-mode . wb/org-mode-visual-fill))
-
-;; -------------------------------------------------------------------------------------
-
-;; -------------------------------------------------------------------------------------
-;; IDE functionality
-
-(use-package projectile
-  :diminish projectile-mode
-  :config (projectile-mode)
-  :custom
-  (setq projectile-indexing-method 'alien
-	projectile-sort-order 'default
-	projectile-enable-caching t
-	projectile-completion-system 'ivy)
-  :bind (("C-c p" . projectile-command-map))
-  :init
-  (when (file-directory-p "~/git/")
-    (setq projectile-project-search-path '("~/git/")))
-  (setq projectile-switch-project-action #'projectile-dired))
-
-(use-package counsel-projectile
-  :config (counsel-projectile-mode))
-
-(use-package magit
-  :custom
-  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
-
-;; ==================
-;; Code completion, indexing, intelli-sense, etc.
-
-(use-package flycheck
-  :init (global-flycheck-mode))
-
-(defun wb/lsp-mode-setup ()
-  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
-  (lsp-headerline-breadcrumb-mode)
-  (setq lsp-modeline-code-actions-segments '(count icon name))
-  (setq lsp-modeline-diagnostics-enable t))
-
-(use-package lsp-mode
-  :init
-  (setq lsp-keymap-prefix "C-c l")
-  :commands lsp
-  :hook ((lsp-mode . wb/lsp-mode-setup)
-	 (csharp-mode . lsp-deferred)
-	 (lsp-mode . lsp-enable-which-key-integration)))
-
-(use-package lsp-ui :commands lsp-ui-mode)
-(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
-(use-package lsp-treemacs
-  :commands lsp-treemacs-errors-list
-  :init
-  (lsp-treemacs-sync-mode 1))
-
-;; optionally if you want to use debugger
-(use-package dap-mode)
-;; (use-package dap-LANGUAGE) to load the dap adapter for your language
-
-(use-package company
-  :after lsp-mode
-  :hook (lsp-mode . company-mode)
-  :bind (:map company-active-map
-	       ("<tab>" . company-complete-selection))
-         (:map lsp-mode-map
-	       ("<tab>" . company-indent-or-complete-common))
-  :custom
-  (setq company-minimum-prefix-length 1
-	company-idle-delay 0.0) ;; default is 0.2
+  (helm-mode 1) ;; Most of Emacs prompts become helm-enabled
+  (helm-autoresize-mode 1) ;; Helm resizes according to the number of candidates
+  (global-set-key (kbd "C-x b") 'helm-buffers-list) ;; List buffers ( Emacs way )
+  (global-set-key (kbd "C-x r b") 'helm-bookmarks) ;; Bookmarks menu
+  (global-set-key (kbd "C-x C-f") 'helm-find-files) ;; Finding files with Helm
+  (global-set-key (kbd "M-c") 'helm-calcul-expression) ;; Use Helm for calculations
+  (global-set-key (kbd "C-s") 'helm-occur)  ;; Replaces the default isearch keybinding
+  (global-set-key (kbd "C-h a") 'helm-apropos)  ;; Helmized apropos interface
+  (global-set-key (kbd "M-x") 'helm-M-x)  ;; Improved M-x menu
+  (global-set-key (kbd "M-y") 'helm-show-kill-ring)  ;; Show kill ring, pick something to paste
   )
 
-(use-package company-box
-  :hook (company-mode . company-box-mode))
+(use-package expand-region
+  :bind
+  ("C-=" . er/expand-region)
+  ("C--" . er/contract-region))
 
-;; ==================
+(use-package company
+  :init
+  (add-hook 'after-init-hook 'global-company-mode))
 
-;; ==================
-;; Specific support for various programming languages
-
-;; ````````````` Markup
-;; ````````````````````
-
-;; ````````````` XML
-;; `````````````````
-
-;; ````````````` C#
-(use-package csharp-mode
-  :mode "\\.cs\\'"
+(use-package org
   :config
-  (setq csharp-indent-level 4))
-;; ````````````````
+  (setq org-support-shift-select t
+	org-src-tab-acts-natively t))
 
-;; ````````````` Rust
-;; ``````````````````
+(use-package org-bullets
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
-;; ````````````` Docker
-;; ````````````````````
+(use-package magit)
 
-;; ````````````` YAML
-;; ``````````````````
+;; Project functionality
+(use-package projectile
+  :config
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (projectile-mode +1)
+  (setq projectile-enable-caching t)
+  (setq projectile-indexing-method 'alien)
+  (setq projectile-globally-ignored-file-suffixes
+	'("#" "~" ".swp" ".o" ".so" ".exe" ".dll" ".elc" ".pyc" ".jar"))
+  (setq projectile-globally-ignored-directories
+	'(".git" "node_modules" "__pycache__" ".vs"))
+  (setq projectile-globally-ignored-files '("TAGS" "tags" ".DS_Store")))
 
-;; ==================
+(use-package helm-projectile
+  :config (helm-projectile-on))
 
-;; -------------------------------------------------------------------------------------
+;; Project structure tree view
+(use-package treemacs
+  :bind
+  (:map global-map
+	([f8] . treemacs)
+	("C-<f8>" . treemacs-select-window))
+  :config
+  (setq treemacs-is-never-other-window t))
+
+(use-package treemacs-projectile
+  :after treemacs projectile)
+
+(use-package flycheck
+  :init
+  ;;(setq flycheck-markdown-markdownlint-cli-executable "markdownlint")
+  (global-flycheck-mode))
+
+;; C# support
+(use-package csharp-mode)
+(add-hook 'csharp-mode-hook 'imenu-add-menubar-index)
+(add-hook 'csharp-mode-hook 'flymake-mode)
+
+;; DotNet support
+(use-package dotnet)
+(add-hook 'csharp-mode-hook 'dotnet-mode)
+(add-hook 'fsharp-mode-hook 'dotnet-mode)
+
+;; Dockerfile support
+(use-package dockerfile-mode
+  :mode (("Dockerfile\\'" . dockerfile-mode)))
+;; Markdown support
+(use-package markdown-mode
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+	 ("\\.md\\'" . markdown-mode)
+	 ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
+(use-package web-mode)
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
+(setq web-mode-engines-alist
+      '(("csharp"  . "\\.as[cp]x\\.")))
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 4)
+  (setq web-mode-css-indent-offset 4)
+  (setq web-mode-code-indent-offset 4)
+  )
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+;; Code snippets
+(use-package yasnippet
+  :config
+  (yas-global-mode 1))
+(add-hook 'prog-mode-hook 'yas-minor-mode)
